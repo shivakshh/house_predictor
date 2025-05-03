8b993ab57d8ca113a6c615252d03ca7f83be1899
Shivaksh <47975586+shivakshh@users.noreply.github.com>
2025-05-03 17:54:34 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import util
+import os
 
 app = Flask(__name__)
 
@@ -29,4 +30,4 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run()
+    app.run(host='0.0.0.0', port=5050)
